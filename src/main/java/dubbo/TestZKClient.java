257792a4af82da2e35c1c5ecad8209f90db959f4
wangyonghua <1770220534@qq.com>
2017-07-02 22:32:08 +0800

    dubbo 测试项目

---
+++ src/main/java/dubbo/TestZKClient.java
@@ -0,0 +1,12 @@
+package dubbo;
+
+import org.I0Itec.zkclient.ZkClient;
+
+public class TestZKClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         ZkClient client=new ZkClient("");
+	}
+
+}
