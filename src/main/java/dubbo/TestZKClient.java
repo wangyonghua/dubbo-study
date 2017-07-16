2243bf44a3d6705eb2045a6c034d15b48585e67b
wangyonghua <1770220534@qq.com>
2017-07-16 13:40:58 +0800

    zkclient监听测试

--- src/main/java/dubbo/TestZKClient.java
+++ src/main/java/dubbo/TestZKClient.java
@@ -1,12 +1,27 @@
 package dubbo;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.I0Itec.zkclient.IZkChildListener;
 import org.I0Itec.zkclient.ZkClient;
 
 public class TestZKClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-         ZkClient client=new ZkClient("");
-	}
+		ZkClient client = new ZkClient("192.168.31.226:2181");
+		// client.createPersistent("/parent1", "1212");
+		client.subscribeChildChanges("/parent", new IZkChildListener() {
 
+			// @Override
+			public void handleChildChange(String arg0, List<String> arg1) throws Exception {
+				System.out.println("变化了");
+				System.out.println(arg1);
+			}
+		});
+		System.out.println("运行完成");
+		System.in.read();
+		client.close();
+	}
 }
